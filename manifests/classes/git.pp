71157b0fa9f8ed99a040312dff69695d429f3813
Alban Peignier <alban@tryphon.eu>
2011-10-18 08:51:57 +0200

    Move git::common in a dedicated file

--- manifests/classes/git.pp
+++ manifests/classes/git.pp
@@ -1,21 +1,3 @@
-class git::common {
-
-  package { git-core: 
-    alias => git,
-    ensure => latest
-  }
-
-  if $debian::lenny {
-    include apt::backports
-    apt::preferences { "git":
-      package => "git", 
-      pin => "release a=lenny-backports",
-      priority => 999,
-      before => Package[git-core]
-    }
-  }
-}
-
 class git {
   include git::common
   include git::storage
